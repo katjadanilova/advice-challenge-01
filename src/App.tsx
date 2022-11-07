bd6516ccbcb8a1a15708569e957b634a4701aed0
katjadanilova <41726243+katjadanilova@users.noreply.github.com>
2022-11-07 21:49:56 +0200

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import dice from "./dice.svg"
 import './App.css';
+import {QueryClient, QueryClientProvider, useQuery, useQueryClient} from "react-query";
+
+
+function Card({title, text, onRefresh}: {
+    title: string, text: string, onRefresh: () => void
+}) {
+    return <div className="Quote-card">
+        <h3>{title}</h3>
+        <div className="quote">{text}</div>
+        <img src={dice} className="dice" alt="logo" onClick={onRefresh}/>
+    </div>
+}
+
+function CardController() {
+
+    const {
+        isLoading: adviceIsLoading,
+        isFetching: adviceIsFetching,
+        isError,
+        data: advice,
+        refetch
+    } = useQuery("get.advice", async () => {
+        const response = await fetch("https://api.adviceslip.com/advice");
+        return response.json()
+    })
+
+    if (adviceIsLoading || adviceIsFetching) {
+        return <div>loading some advice...</div>
+    }
+
+    if (isError)
+        return <div>Error loading advice...</div>
+
+    async function onRefresh() {
+        await refetch();
+    }
+
+    return <Card title={advice.slip.id}
+                 text={advice.slip.advice}
+                 onRefresh={onRefresh}
+    />
+
+}
+
+
+const client = new QueryClient();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <QueryClientProvider client={client}>
+            <div className="App">
+                <CardController/>
+            </div>
+        </QueryClientProvider>
+    );
 }
 
 export default App;
